ae11ce5d2a429be54cd7ffc6190ca20e964023ea
Andie <zchangan@163.com>
2017-08-04 11:03:17 +0800

    69

---
+++ Easy/69.Binary Tree Level Order Traversal.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+    * @param root: The root of binary tree.
+    * @return: Level order a list of lists of integer
+    */
+public:
+    vector<vector<int>> levelOrder(TreeNode *root) {
+        // write your code here
+        vector<vector<int>> ans;
+        level(root, 0, ans);
+        return ans;
+    }
+
+    void level(TreeNode *root, int depth, vector<vector<int>> & ans) {
+        if (!root) return;
+        if (ans.size() <= depth) ans.push_back(vector<int>());
+        vector<int> &current = ans[depth];
+        current.push_back(root->val);
+        level(root->left, depth + 1, ans);
+        level(root->right, depth + 1, ans);
+    }
+};
