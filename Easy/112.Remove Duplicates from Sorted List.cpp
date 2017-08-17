2b0ef82f3dde06a4d53e98eff64271bd5f989d52
Andie <zchangan@163.com>
2017-08-18 07:48:08 +0800

    112

---
+++ Easy/112.Remove Duplicates from Sorted List.cpp
@@ -0,0 +1,31 @@
+/**
+* Definition of ListNode
+* class ListNode {
+* public:
+*     int val;
+*     ListNode *next;
+*     ListNode(int val) {
+*         this->val = val;
+*         this->next = NULL;
+*     }
+* }
+*/
+class Solution {
+public:
+    /**
+    * @param head: The first node of linked list.
+    * @return: head node
+    */
+    ListNode *deleteDuplicates(ListNode *head) {
+        // write your code here
+        ListNode *cur = head;
+        while (cur && cur->next) {
+            if (cur->val == cur->next->val) {
+                cur->next = cur->next->next;
+            } else {
+                cur = cur->next;
+            }
+        }
+        return head;
+    }
+};
